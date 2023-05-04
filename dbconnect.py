3344560ba11bd4224305edf38208ac8f670bc681
malims <raphaelmalimsj@gmail.com>
2023-05-04 12:35:30 +0300

    Data Quality check changes

--- dbconnect.py
+++ dbconnect.py
@@ -1,5 +1,11 @@
 import os
 import psycopg2
+from dataqualitychecks import check_for_nulls
+from dataqualitychecks import check_for_min_max
+from dataqualitychecks import check_for_valid_values
+from dataqualitychecks import check_for_duplicates
+from dataqualitychecks import run_data_quality_check
+
 
 pgpassword = os.environ.get('POSTGRES_PASSWORD')
 conn = None
@@ -19,3 +25,11 @@ finally:
 	if conn:
 		conn.close()
 		print("Connection closed")
+
+#start of data quality checks
+results = []
+tests = {key:value for key,value in mytests.__dict__.items() if key.startswith('test')}
+for testname,test in tests.items():
+	test['conn'] = conn
+	results.append(run_data_quality_check(**test))
+	
